d2078e903e06475b35806f0d487a2d3bcab593e6
mateopinto0 <pintomateo74@gmail.com>
2026-06-09 09:13:22 -0300

    agregado de form

--- src/components/adminComponents/ProductFormUI.jsx
+++ src/components/adminComponents/ProductFormUI.jsx
@@ -1,37 +1,38 @@
+import "./ProductForm.css"
 export const ProductFormUI = ({product,errores,loading,onChange,onFileChange,onSubmit}) =>{
 
     return(
-        <div>
-            <form onSubmit={onSubmit}>
+        <div className="container">
+            <form onSubmit={onSubmit} className="form">
                 <h2>Añadir Juego</h2>
 
-                <div>
+                <div className="input">
                 <label>Nombre:</label>
                 <input type="text" name="nombre" value={product.nombre} onChange={onChange} required></input>
                 {errores.nombre && <p>{errores.nombre}</p>}
                 </div>
-                <div>
+                <div className="input">
                 <label>Descripcion:</label>
                 <input type="text" name="descripcion" value={product.descripcion} onChange={onChange} required></input>
                 {errores.descripcion && <p>{errores.descripcion}</p>}
                 </div>
-                <div>
+                <div className="input">
                 <label>Precio:</label>
                 <input type="number" name="precio" value={product.precio} onChange={onChange} required></input>
                 {errores.precio && <p>{errores.precio}</p>}
                 </div>
-                <div>
+                <div className="input">
                 <label>Cantidad:</label>
                 <input type="number" name="cantidad" value={product.cantidad} onChange={onChange} required></input>
                 {errores.cantidad && <p>{errores.cantidad}</p>}
                 </div>
 
-                <div>
+                <div className="input">
                     <label>Imagen:</label>
                     <input type="file" accept="image/*" onChange={onFileChange} required></input>
                     {errores.imagenUrl && <p>{errores.imagenUrl}</p>}
                 </div>
-                <button type="submit">Añadir Juego</button>
+                <button type="submit" className="button btn-agregar">Añadir Juego</button>
 
                 {errores.general && <p>{errores.general}</p>}
             </form>
--- src/components/adminComponents/ProductsSuccess.jsx
+++ src/components/adminComponents/ProductsSuccess.jsx
@@ -1,16 +1,17 @@
 import { useNavigate, useParams } from "react-router-dom"
+import "./ProductSuccess.css"
 
 export const ProductSuccess = () => {
     const {id} = useParams();
     const navigate = useNavigate();
 
     return(
-        <div>
+        <div className="container-success">
             <h2>Juego cargado con exito!</h2>
             <p>Id de juego: {id}</p>
             <p>Puede cargar otro haciendo click en el boton</p>
 
-            <button onClick={()=> navigate("/admin",{replace:true})}>
+            <button className="button" onClick={()=> navigate("/admin",{replace:true})}>
             Agregar otro Juego
             </button>
         </div>
